b7c30ac72d853dcc0af784522713b0a713249434
lauranacher-lang <laura.nacher@fandango.it>
2026-07-07 15:55:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const APPS_SCRIPT_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnT3c2Fq8lyzz14znASb8FL6VV3-KJGHQVqEcD78WFKN_x0mDbZ05m2msTrX4EKez2UY-JDOvQqenCaRSfY_AafYDcqzPmq1Vn3nFEq19nEkcBhu8GqOOHE8YOF9fa0kWb0wCLE3zL7P96N0czBu9AZWWeKda7OBsyllJbxZWYBC_t5hT4eoP5Q0Ld2HQKGxBbat5QVgGi99YTO_iMGL9yR3pfilvNsnHW5B6Dr8nNDq6XwOq_0NV0Bu_OXWvc4fgQBh-Kuqku5k5NnTO37Gh1jKaJr4wQ&lib=M35mnJW96LB7gYUuIVOUOPefSiK4m_T-y";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwIGgb__GwVegpRQnjIEjs5ytb1B7nyWxCKw1AuHP4bpvfAlFUTDfJ16rfd1GrGyAtSGA/exec";
 
 app.get('/api/deals', async (req, res) => {
   try {
